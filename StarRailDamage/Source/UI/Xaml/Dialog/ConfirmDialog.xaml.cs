06545ea16fb1d429673433cf205dca934e7413c3
Trankite <Trankite@icloud.com>
2026-06-02 11:59:12 +0800

    确认取消选择窗口

---
+++ StarRailDamage/Source/UI/Xaml/Dialog/ConfirmDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using StarRailDamage.Source.UI.Xaml.Control.Panel;
+using System.Windows;
+
+namespace StarRailDamage.Source.UI.Xaml.Dialog
+{
+    public partial class ConfirmDialog : ScopedDialog
+    {
+        public ConfirmDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAcceptClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void OnCancelClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(nameof(Header), typeof(string), typeof(ConfirmDialog));
+
+        public string Message
+        {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+
+        public static readonly DependencyProperty MessageProperty = DependencyProperty.Register(nameof(Message), typeof(string), typeof(ConfirmDialog));
+    }
+}
