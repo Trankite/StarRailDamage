2d763fe3664a81080b777b06b7fafef610f29cb4
Trankite <2798220344@qq.com>
2026-06-02 11:59:12 +0800

    确认取消选择窗口

---
+++ StarRailDamage/Source/UI/Xaml/Control/Panel/ScopedDialog.cs
@@ -0,0 +1,7 @@
+﻿namespace StarRailDamage.Source.UI.Xaml.Control.Panel
+{
+    public abstract class ScopedDialog : ScopedWindow
+    {
+
+    }
+}
---
+++ StarRailDamage/Source/UI/Xaml/Page/QRCodeLogin.xaml.cs
@@ -0,0 +1,12 @@
+﻿using StarRailDamage.Source.UI.Xaml.Control.Panel;
+
+namespace StarRailDamage.Source.UI.Xaml.Page
+{
+    public partial class QRCodeLogin : ScopedPage
+    {
+        public QRCodeLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
